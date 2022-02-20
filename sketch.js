262b48bb37cd7632639e57f9c6bc299bb6799bd5
HaaziqAkbar <99976687+HaaziqAkbar@users.noreply.github.com>
2022-02-20 12:34:43 +0530

    Add files via upload
    hi, this is a background color changer press up, down, right and left arrow key to change background color.

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,300,);
+
+}
+
+function draw() 
+{
+   background(150);
+
+  box.position.x = box.position.x = -3;
+  box.position.y = box.position.y = +1;
+
+drawSprites();
+}
